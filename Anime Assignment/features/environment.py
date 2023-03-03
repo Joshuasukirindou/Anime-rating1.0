06baab02afeaba1efb78344d4e6d36093706a16e
Joshuasukirindou <zhangsunchuanl@gmail.com>
2023-03-03 16:12:22 +0000

    assignment2.2

---
+++ Anime Assignment/features/environment.py
@@ -0,0 +1,33 @@
+# Produce by Joshua
+
+# University of Aberdeen
+
+# Development time: 2023/3/3 15:59
+
+# from behave import given, when, then
+#
+#
+# @given(u'I navigate to the anime page')
+# def nav(context):
+#     """"""
+#     'Navigate to the anime pages'
+#     """"""
+#     context.browser.get('http://localhost:5000/Anime_rating')
+#
+#
+# @when(u'I click on the link to Anime details')
+# def click(context):
+#     """"""
+#     'Find the desire link'
+#     """"""
+#     context.browser.find_element_by_partial_link_text('2').click()
+#
+#
+# @then(u'I should see the grader for that anime')
+# def grader(context):
+#     """"""
+#     'If successful, then we should be directed to the grader page'
+#     """"""
+#     # use print(context.browser.page_source) to aid debugging
+#     print(context.)
+
