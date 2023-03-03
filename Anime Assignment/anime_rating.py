ddedc4706d071678afd6f0921648631850041826
Joshuasukirindou <zhangsunchuanl@gmail.com>
2023-03-03 03:03:54 +0000

    assignment2.0

--- Anime Assignment/anime_rating.py
+++ Anime Assignment/anime_rating.py
@@ -7,18 +7,37 @@
 import sqlite3
 from flask import Flask, render_template
 
-app= Flask(__name__)
+app = Flask(__name__)
+
+# database details: What this sentence(code) does is to remove some duplication
+db_name = 'Anime_search_data.db'
 
 
 @app.route('/')
 def index():
-    conn = sqlite3.connect('Anime_search_data.db')
+    return render_template('Anime_index.html')
+
+
+@app.route('/Anime_details')
+def animes_details():
+    conn = sqlite3.connect(db_name)
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
-    cur.execute("select * from deployments")
+    cur.execute("select * from Anime")
     rows = cur.fetchall()
     conn.close()
-    return render_template('Anime_index.html', rows=rows)
+    return render_template('Anime_details.html', rows=rows)
+
+@app.route('/User_rating')
+def user_rating():
+    conn = sqlite3.connect(db_name)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    # get result from user
+    cur.execute("select * from User_rating")
+    rows = cur.fetchall()
+    conn.close()
+    return render_template('User_rating.html',rows=rows)
 
 
 if __name__ == '__main__':
---
+++ Anime Assignment/setup.py
@@ -0,0 +1,58 @@
+# Produce by Joshua
+
+# University of Aberdeen
+
+# Development time: 2023/2/27 14:58
+
+import csv
+import sqlite3
+import decimal
+from datetime import datetime
+
+
+# open the connection to the database
+
+conn = sqlite3.connect('Anime_search_data.db',detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+# create cursor to select files or values
+cur = conn.cursor()
+
+# drop the tables to clean repeating values
+conn.execute('DROP TABLE IF EXISTS Anime')
+conn.execute('DROP TABLE IF EXISTS User_rating')
+
+print("table dropped successfully");
+
+conn.execute('CREATE TABLE Anime(id INTEGER PRIMARY KEY, Anime_name TEXT, Anime_type TEXT, Anime_Rating INTEGER )')
+conn.execute('CREATE TABLE User_rating(id INTEGER, Anime_id INTEGER, rating INTEGER, FOREIGN KEY(Anime_id) REFERENCES Anime(id))')
+print("table created successfully");
+
+with open('Anime csv files/anime3.csv',encoding='ISO-8859-1', newline='') as file1:
+    reader1 = csv.reader(file1, delimiter=",")
+    next(reader1)
+    for row in reader1:
+
+        Anime_id = int(row[0])
+        Anime_name = row[1]
+        Anime_type = row[3]
+        Anime_rating = float(row[5])
+
+        cur.execute('INSERT INTO Anime VALUES (?,?,?,?)', (Anime_id, Anime_name, Anime_type, Anime_rating))
+        conn.commit()
+
+with open('Anime csv files/rating.csv',encoding='ISO-8859-1', newline='') as file2:
+    reader2 = csv.reader(file2, delimiter=",")
+    next(reader2)
+    for row in reader2:
+        User_id = int(row[0])
+        Anime_id = int(row[1])
+        rating = int(row[2])
+
+        cur.execute('INSERT INTO User_rating VALUES(?,?,?)', (User_id, Anime_id, rating))
+        conn.commit()
+
+    print("data parsed successfully");
+
+    conn.close()
+
+
+
